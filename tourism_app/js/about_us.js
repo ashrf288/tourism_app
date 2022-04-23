03128be52a085d1a9e6df21c7f18742683348603
ashrf obeidat <ashrfmathkour@gmail.com>
2022-04-23 11:20:04 +0300

    about us

--- tourism_app/js/about_us.js
+++ tourism_app/js/about_us.js
@@ -1,16 +1,35 @@
-﻿
-
+﻿let auth_user = {
+  userName: "ashrf",
+  password: "1234567",
+};
 let form = document.getElementById("contact_form");
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    data = {
-        email: e.target.email.value,
-        password: e.target.password.value,
-        address: e.target.address.value,
-        city: e.target.city.value,
-        gender: e.target.gender.value,
-        phone: e.target.phone.value,
-        terms:e.target.terms.value,
-    }
-    console.log(data)
-})
+  e.preventDefault();
+  data = {
+    email: e.target.email.value,
+    userName: e.target.userName.value,
+    password: e.target.password.value,
+    address: e.target.address.value,
+    city: e.target.city.value,
+    gender: e.target.gender.value,
+    phone: e.target.phone.value,
+    terms: e.target.terms.value,
+  };
+  console.log(data);
+
+  if (
+    auth_user.userName == data.userName &&
+    auth_user.password == data.password
+  ) {
+    document.querySelector(".submit_result").innerHTML = `
+      <div class="card  info-card  ">
+      <div class="card-body">
+        <h2 class="card-title">auth result </h2><hr>
+        <p class="card-text"><strong> form has been submitted succssfully </strong> </p>
+      </div>
+    </div>
+      `;
+  } else {
+    console.log("not auth");
+  }
+});
